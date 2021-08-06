247d45eb9fe0094b905ab51da8b9819557012683
Cedric <cedrichartskeerl@gmail.com>
2021-08-06 12:53:06 +0200

    Implement code and tests

--- lib/ch_palindrome.rb
+++ lib/ch_palindrome.rb
@@ -2,7 +2,28 @@
 
 require_relative "ch_palindrome/version"
 
-module ChPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  # Returns string without punctuation
+  # def letters
+  # 	# self.chars.select{|n| n.match(/[a-z]/i)}.join
+  # 	self.scan(/[a-z]/i).join
+  # end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+    	# self.letters.downcase
+      self.scan(/[a-z]/i).join.downcase
+    end
+
+
 end
+
+# Madam, I'm Adam.
--- test/ch_palindrome_test.rb
+++ test/ch_palindrome_test.rb
@@ -7,7 +7,25 @@ class ChPalindromeTest < Minitest::Test
     refute_nil ::ChPalindrome::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  # def test_letters
+  #   assert_equal "MadamImAdam", "Madam, I'm Adam.".letters
+  # end
+
 end
