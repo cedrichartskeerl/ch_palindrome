c6d6ccb13245066c8e29e7d55f916b3dac2a728b
Cedric <cedrichartskeerl@gmail.com>
2021-08-09 15:01:02 +0200

    put palindrome test in module

--- lib/ch_palindrome.rb
+++ lib/ch_palindrome.rb
@@ -1,16 +1,23 @@
 require "ch_palindrome/version"
 
+module ChPalindrome
+	# Returns true for a palindrome, false otherwise.
+	def palindrome?
+	  processed_content == processed_content.reverse
+	end
+
+	private
+
+	  # Returns content for palindrome testing.
+	  def processed_content
+	    self.to_s.scan(/[a-z]/i).join.downcase
+	  end
+end
+
 class String
+	include ChPalindrome
+end
 
-  # Returns true for a palindrome, false otherwise.
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  private
-
-    # Returns content for palindrome testing.
-    def processed_content
-      self.to_s.scan(/[a-z]/i).join.downcase
-    end
+class Integer
+	include ChPalindrome
 end
